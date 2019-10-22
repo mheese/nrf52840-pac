b704a931f4d952c1a5298518be2db81d873a90b3
Marcus Heese <marcus.heese@gmail.com>
2019-10-21 22:58:31 -0700

    cleanup in update.sh

--- update.sh
+++ update.sh
@@ -4,9 +4,7 @@ set -e
 
 # NOTE: Last executed using Rust 1.38.0
 
-#cargo install --force --version 0.14.0 svd2rust
 cargo install --force --version 0.16.1 svd2rust
-#cargo install --force --version 0.4.0  form
 cargo install --force --version 0.7.0  form
 rustup component add rustfmt
 
